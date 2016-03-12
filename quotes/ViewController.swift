9404990711f19dbfb74c5638202b062853aa246a
Icaro Lavrador <icaro@icaros-imac.telecom>
2016-03-12 21:24:17 +1300

    Changes from developer brunch merged

--- quotes/ViewController.swift
+++ quotes/ViewController.swift
@@ -9,13 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelAction: UILabel!
+    
+    @IBAction func displayNewQuote() {
+        labelAction.text = "Should display a new quote now"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //Changes to developer A brunch
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
