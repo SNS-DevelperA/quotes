813103154cf8ed31af9bc4adeb9342839ca64a08
Icaro Lavrador <icaro@icaros-imac.telecom>
2016-03-12 21:11:48 +1300

    Old Code

--- quotes/ViewController.swift
+++ quotes/ViewController.swift
@@ -10,12 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var labelAction: UILabel!
-    
-    @IBAction func displayNewQuote() {
-        labelAction.text = "Should display a new quote now"
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
